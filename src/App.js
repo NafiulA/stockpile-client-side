066aef478009492f9c9199d676559e332f0acaf6
Nafiul Alam <nafiulalam30@gmail.com>
2022-04-30 16:34:04 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/authentication/Login/Login';
+import Register from './components/authentication/Register/Register';
+import Home from './components/homepage/Home/Home';
+import InventoryItem from './components/homepage/InventoryItem/InventoryItem';
+import AddItem from './components/management/AddItem/AddItem';
+import ManageInventory from './components/management/ManageInventory/ManageInventory';
+import MyItems from './components/management/MyItems/MyItems';
+import Blogs from './components/others/Blogs/Blogs';
+import Contact from './components/others/Contact/Contact';
+import Footer from './components/shared/Footer/Footer';
+import Header from './components/shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/inventory/:id' element={<InventoryItem></InventoryItem>}></Route>
+        <Route path='/manageinventories' element={<ManageInventory></ManageInventory>}></Route>
+        <Route path='/additem' element={<AddItem></AddItem>}></Route>
+        <Route path='/myitems' element={<MyItems></MyItems>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/homepage/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../HeroSection/HeroSection';
+import Inventory from '../Inventory/Inventory';
+
+const Home = () => {
+    return (
+        <div>
+            <HeroSection></HeroSection>
+            <Inventory></Inventory>
+        </div>
+    );
+};
+
+export default Home;
